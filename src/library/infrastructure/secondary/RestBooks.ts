b282840e46de67f0926eb6fc88140f4a1e0ea012
Anthony Rey <gnuk@mailoo.org>
2023-11-13 16:49:09 +0100

    Add isbn as parameter

--- src/library/infrastructure/secondary/RestBooks.ts
+++ src/library/infrastructure/secondary/RestBooks.ts
@@ -3,13 +3,14 @@ import { Either } from '@/functional/Either';
 import { Book } from '@/library/domain/Book';
 import { AxiosInstance } from 'axios';
 import { RestBook, toBook } from '@/library/infrastructure/secondary/RestBook';
+import { ISBN } from '@/library/domain/ISBN.ts';
 
 export class RestBooks implements Books {
   constructor(private readonly axiosInstance: AxiosInstance) {}
 
-  get(): Promise<Either<Error, Book>> {
+  get(isbn: ISBN): Promise<Either<Error, Book>> {
     return this.axiosInstance
-      .get<RestBook>('/isbn/9780321125217.json')
+      .get<RestBook>(`/isbn/${isbn.get()}.json`)
       .then(response => response.data)
       .then(toBook)
       .catch(error => Promise.resolve(Either.err(error)));
