d81f0d12863a04aac19db6c18c037967d120a5d5
Anthony Rey <gnuk@mailoo.org>
2023-11-13 16:49:09 +0100

    Add isbn as parameter

--- src/library/application/LibraryRoutes.tsx
+++ src/library/application/LibraryRoutes.tsx
@@ -1,14 +1,20 @@
-import { RouteObject } from 'react-router-dom';
+import { RouteObject, useParams } from 'react-router-dom';
 import { BookComponent } from '@/library/infrastructure/primary/BookComponent.tsx';
 import { LibraryApp } from '@/library/application/LibraryApp.tsx';
+import { ISBN } from '@/library/domain/ISBN.ts';
+
+const BooksPage = () => {
+  const { isbn } = useParams<string>();
+  return <BookComponent isbn={ISBN.of(isbn!)} />;
+};
 
 export const libraryRoutes: RouteObject = {
   path: '/',
   element: <LibraryApp />,
   children: [
     {
-      path: '',
-      element: <BookComponent />,
+      path: 'book/:isbn',
+      element: <BooksPage />,
     },
   ],
 };
--- src/library/infrastructure/primary/BookComponent.tsx
+++ src/library/infrastructure/primary/BookComponent.tsx
@@ -1,31 +1,87 @@
-import { useState } from 'react';
-import { useLoadEither } from '@/library/infrastructure/primary/UseLoad';
+import { useEffect, useReducer } from 'react';
 import { useTranslation } from 'react-i18next';
 import { inject } from '@/injections.ts';
 import { BOOKS } from '@/library/application/LibraryKeys.ts';
 import { Book } from '@/library/domain/Book.ts';
+import { ISBN } from '@/library/domain/ISBN.ts';
+import { useNavigate } from 'react-router-dom';
 
-export const BookComponent = () => {
+const PageInProgress = Symbol();
+const PageError = Symbol();
+const PageSuccess = Symbol();
+
+type PageStatus = typeof PageError | typeof PageInProgress | typeof PageSuccess;
+
+interface PageState<T> {
+  content?: T;
+  errorMessage?: string;
+  status: PageStatus;
+}
+
+interface TypeAction {
+  type: PageStatus;
+}
+
+interface InProgressAction extends TypeAction {
+  type: typeof PageInProgress;
+}
+
+interface SuccessAction<T> extends TypeAction {
+  type: typeof PageSuccess;
+  content: T;
+}
+
+interface ErrorAction extends TypeAction {
+  type: typeof PageError;
+  message: string;
+}
+
+function pageStateReducer<T>(_: PageState<T>, actions: InProgressAction | SuccessAction<T> | ErrorAction): PageState<T> {
+  switch (actions.type) {
+    case PageSuccess:
+      return { content: actions.content, status: actions.type };
+    case PageInProgress:
+      return { status: actions.type };
+    case PageError:
+      return { errorMessage: actions.message, status: actions.type };
+  }
+}
+
+export const BookComponent = (props: { isbn: ISBN }) => {
   const books = inject(BOOKS);
 
   const { t } = useTranslation();
-  const [book, setBook] = useState<Book>();
+  const navigate = useNavigate();
+  const [page, setPage] = useReducer<typeof pageStateReducer<Book>>(pageStateReducer, { status: PageInProgress });
 
-  const { isInProgress, isFailing, isSuccessful, errorMessage } = useLoadEither(books.get(), book => {
-    setBook(book);
-  });
+  useEffect(() => {
+    setPage({ type: PageInProgress });
+    books
+      .get(props.isbn)
+      .then(either =>
+        either.evaluate(
+          error => setPage({ type: PageError, message: error.message }),
+          content => setPage({ type: PageSuccess, content: content })
+        )
+      )
+      .catch((error: Error) => {
+        setPage({ type: PageError, message: error.message });
+      });
+  }, [props.isbn]);
 
   return (
     <>
-      {isInProgress && <p data-selector="book.loading">{t('book.inProgress')}</p>}
-      {isFailing && <p data-selector="book.error">{errorMessage}</p>}
-      {isSuccessful && (
+      {page.status === PageInProgress && <p data-selector="book.loading">{t('book.inProgress')}</p>}
+      {page.status === PageError && <p data-selector="book.error">{page.errorMessage!}</p>}
+      {page.status === PageSuccess && (
         <ul>
           <li data-selector="book.title">
             <strong data-selector="book.label.title">{t('book.title')} </strong>
-            <span data-selector="book.title">{book?.title}</span>
+            <span data-selector="book.title">{page.content?.title}</span>
           </li>
-          <li data-selector="book.isbn">{book?.isbn.get()}</li>
+          <li data-selector="book.isbn">{page.content?.isbn.get()}</li>
+          <button onClick={() => navigate('/book/9782070541270')}>HP</button>
+          <button onClick={() => navigate('/book/9780321125217')}>DDD</button>
         </ul>
       )}
     </>
