bbd2f68211f68f141494fe11f1216205b850b9c6
Anthony Rey <gnuk@mailoo.org>
2024-08-08 14:59:44 +0200

    Update dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,17 @@ export default defineConfig({
   test: {
     include: ['test/unit/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     environment: 'jsdom',
+    coverage: {
+      include: ['src/**/*'],
+      exclude: [
+        'src/*.{ts,tsx}',
+        '**/index.{ts,tsx}',
+        'src/**/primary/**/*',
+        'src/**/application/**/*',
+        'src/modal/**/*',
+        'src/static/**/*',
+      ],
+    },
   },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
