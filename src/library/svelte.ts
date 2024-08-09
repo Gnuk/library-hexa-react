0a443cfbe4a89c9645b287591dabf7806f861885
Anthony Rey <gnuk@mailoo.org>
2024-08-09 19:04:14 +0200

    Add Svelte

---
+++ src/library/svelte.ts
@@ -0,0 +1,5 @@
+export { libraryProvider } from './application/libraryProvider.ts';
+
+import LibraryApp from "@/library/application/LibraryApp.svelte";
+
+export {LibraryApp};
--- vite-svelte.config.ts
+++ vite-svelte.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import * as path from 'path';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svelte()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   server: {
     port: 3030,
   },
-  envDir: path.resolve(__dirname, 'config', 'react'),
+  envDir: path.resolve(__dirname, 'config', 'svelte'),
   build: {
     rollupOptions: {
-      external: [new RegExp(/\.vue/), new RegExp(/\.svelte/)],
+      external: [new RegExp(/\.vue/)],
     },
   },
 });
